9058733c523d36e266b4a3f0bc5be103659dd671
nasimabo <nasimuddin555k@gmail.com>
2024-02-23 00:33:19 +0600

    Assenment operator

---
+++ Assenment operator .py
@@ -0,0 +1,16 @@
+################ Logical Operator ###################
+
+num = int(input("Enter the number : "))
+
+num += 10
+print(num)
+num -= 10
+print(num)
+num *= 5
+print(num)
+num /= 5
+print(num)
+num %= 7
+print(num)
+num **= 3
+print(num)
